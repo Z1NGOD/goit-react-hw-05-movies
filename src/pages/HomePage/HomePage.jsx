daf742813552ac031974fe200ea12f92d5b6c249
Z1NG0D <YaroslavBorovskiy@gmail.com>
2023-08-27 19:53:47 +0200

    comit

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,18 +1,12 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
 import Home from 'components/Home/Home';
 
 function HomePage(props) {
   return (
     <div>
-      <Home/>
+      <Home />
     </div>
   );
 }
 
-HomePage.propTypes = {
-
-}
-
-export default HomePage
-
+export default HomePage;
